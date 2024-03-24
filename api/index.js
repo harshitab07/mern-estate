a874ec49a7226222d1f59a6a28549ac721fe4b16
harshitab07 <harshitabhatt1730@gmail.com>
2024-03-24 20:05:29 +0530

    db connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+// read env variables
+dotenv.config();
+
+// start server
 const app = express();
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
+});
+
+// connect database
+const mongo_url = process.env.MONGO_URL;
+mongoose.connect(mongo_url).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log('Error in connection with DB', err);
 });
