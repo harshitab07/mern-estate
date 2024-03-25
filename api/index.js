d0e47f567780b870b969ea9740a648809ce37a1d
harshitab07 <harshitabhatt1730@gmail.com>
2024-03-25 12:05:51 +0530

    signup API created

--- api/index.js
+++ api/index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 // read env variables
 dotenv.config();
 
 // start server
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
@@ -18,4 +22,19 @@ mongoose.connect(mongo_url).then(() => {
     console.log('Connected to MongoDB');
 }).catch((err) => {
     console.log('Error in connection with DB', err);
+});
+
+// api end points
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+// error handling middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
 });
